9ae9fb3f0ea1c4845fc84fdd88774fec23076749
“preetimca2016” <preetimca2016@gmail.com>
2022-08-22 00:20:51 +0530

    Add: UC1- Create Contact

---
+++ AddressBook/AddressBook.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    class AddressBook
+    {
+        //Intialisation
+        public string FirstName;
+        public string LastName;
+        public string Address;
+        public string City;
+        public string State;
+        public int ZipCode;
+        public long PhoneNumber;
+        public string Email;
+
+        //use parameterised constructor
+        public AddressBook(string firstName, string lastName, string address, string city, string state, int zipCode, long phoneNumber, string email)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Address = address;
+            this.City = city;
+            this.State = state;
+            this.ZipCode = zipCode;
+            this.PhoneNumber = phoneNumber;
+            this.Email = email;
+        }
+        public void display()
+        {
+            Console.WriteLine("First Name is:- " + this.FirstName);
+            Console.WriteLine("Last Name is:- " + this.LastName);
+            Console.WriteLine("Address is:- " + this.Address);
+            Console.WriteLine("City is:- " + this.City);
+            Console.WriteLine("State is:- " + this.State);
+            Console.WriteLine("ZipCode is:- " + this.ZipCode);
+            Console.WriteLine("PhoneNumber is:- " + this.PhoneNumber);
+            Console.WriteLine("Email is:- " + this.Email);
+            Console.WriteLine("-------------------------------------------------------------------");
+        }
+
+    }
+}
+
---
+++ AddressBook/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book program");
+            ///UC1 Create Contact
+            AddressBook addressBook = new AddressBook("Preeti", "Sharma", "xyz", "Ggn", "Haryana", 122001, 1234567890, "acd@gmail.com");
+            addressBook.display();
+        }
+    }
+}
+
+        
