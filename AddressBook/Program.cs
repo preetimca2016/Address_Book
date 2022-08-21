0e01cc6e067fce556e8113ee604be69d3b248950
“preetimca2016” <preetimca2016@gmail.com>
2022-08-22 00:30:07 +0530

    Add:UC2-Add contacts

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -7,9 +7,32 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book program");
-            ///UC1 Create Contact
+            ///UC1 Create Contacts
             AddressBook addressBook = new AddressBook("Preeti", "Sharma", "xyz", "Ggn", "Haryana", 122001, 1234567890, "acd@gmail.com");
             addressBook.display();
+            //UC2 Add contacts
+            Console.WriteLine("Enter details to add a new contact");
+            Console.WriteLine("Enter First Name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter Address");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter City ");
+            string city = Console.ReadLine();
+            Console.WriteLine("Enter State");
+            string state = Console.ReadLine();
+            Console.WriteLine("Enter ZipCode");
+            int zipCode = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Phone number");
+            long phoneNumber = long.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Email");
+            string email = Console.ReadLine();
+
+            AddressBook addressBook1 = new AddressBook(firstName, lastName, address,
+                city, state, zipCode, phoneNumber, email);
+            addressBook1.display();
+
         }
     }
 }
